ff05380e11871ca5ab42fd248fc1098fb7addaac
Rimpa Adhikary <141876300+Rimpa-Adhikary@users.noreply.github.com>
2025-12-14 03:53:21 +0530

    Add files via upload

---
+++ MATLAB/switchCase.m
@@ -0,0 +1,15 @@
+x = input("Enter the number :");
+switch x
+    case 1
+        fprintf("A");
+    case 2
+        fprintf("B");
+    case 3
+        fprintf("C");
+    case 4
+        fprintf("D");
+    case 5
+        fprintf("E");
+    otherwise
+        fprintf("Out of Range");
+end
